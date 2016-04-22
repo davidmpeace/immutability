e0fa25cbde044d5ac449c61274e37e4ee79c06d5
David Peace <david@zingle.me>
2016-04-22 09:38:12 -0700

    fixing issues, and verifying functionality

--- src/Immutability.php
+++ src/Immutability.php
@@ -7,48 +7,99 @@ use Eloquent\Attributes\Exceptions\ImmutableFieldViolationException;
 trait Immutability
 {
     /**
-     * Each model should populate this array with their specific immutable fields.
-     * @var array
-     */
-    protected $immutable = []; 
-
-    /**
-     * Registers a saving event, that will fire an exception if an immutable field got past, and is trying to save when dirty.
+     * Registers a saving event, that will fire an exception if an immutable field got past our setter validation, 
+     * and is trying to save the changed value.
+     * 
      * @return null
      */
     protected static function bootImmutability()
     {
         static::saving(function ($model) {
-            $primaryKey      = $model->primaryKey;
-            $primaryKeyValue = @$model->attributes[$primaryKey];
+            $immutable = $model->getImmutableAttributes();
 
-            // Only check, if we have a primary key value set
-            if( !empty($primaryKeyValue) ) {
-                foreach( $model->immutable as $attribute ) {
-                    if( $model->isDirty($attribute) ) {
-                        throw new ImmutableFieldViolationException("Immutable attribute `$attribute` may not be changed.");
-                    }
+            foreach( $immutable as $attribute ) {
+                if( $model->violatesImmutability($attribute) ) {
+                    throw new ImmutableFieldViolationException("`".get_class($model)."` Save Exception: Immutable attribute `$attribute` may not be changed.");
                 }
             }
         });
     }
 
     /**
-     * Catch all before setters are called, to ensure we aren't setting the value for an immutable attribute.
+     * This catch all will be called before any other setters are called, to ensure we aren't setting the value for 
+     * an immutable attribute.
+     * 
      * @param string $key
      * @param mixed $value
      * @return mixed
      */
     public function setAttribute($key, $value)
     {
-        $primaryKey      = $this->primaryKey;
-        $primaryKeyValue = @$this->attributes[$primaryKey];
-
-        if( !empty($primaryKeyValue) && in_array($key, $this->immutable) ) {
+        if( $this->violatesImmutability($key, $value) ) {
             throw new ImmutableFieldViolationException("Immutable attribute `$key` may not be changed.");
         }
 
         return parent::setAttribute($key, $value);
     }
-}
 
+    /**
+     * Checks the model for $immutable attributes array, and returns an array of immutable attribute names.
+     * 
+     * @return array
+     */
+    protected function getImmutableAttributes()
+    {
+        $immutable = (isset($this->immutable)) ? $this->immutable : [];
+        return $immutable;
+    }
+
+    /**
+     * Returns true if an attribute is immutable.
+     * 
+     * @param  string  $attribute
+     * @return boolean
+     */
+    protected function isImmutable( $attribute )
+    {
+        $immutable   = $this->getImmutableAttributes();
+        $isImmutable = in_array($attribute, $immutable);
+        return $isImmutable;
+    }
+
+    /**
+     * Returns true if an attribute has been set erroneously, and violated immutability.
+     * 
+     * @param  string $attribute
+     * @param  mixed  $newValue     This is only passed, if the value has not been set on the model yet.
+     * @return boolean
+     */
+    protected function violatesImmutability( $attribute, $newValue = null )
+    {
+        $violated = false;
+
+        if( $this->exists && $this->isImmutable($attribute) && !$this->bypassImmutabilityCheck($attribute) ) {
+            if( func_num_args() == 2 ) {
+                if( array_key_exists($attribute, $this->attributes) ) {
+                    $currentValue = array_get($this->attributes, $attribute);
+                    $violated     = ($newValue != $currentValue);
+                }
+            } else {
+                $violated = $this->isDirty($attribute);
+            }
+        }
+
+        return $violated;
+    }
+
+    /**
+     * This method can be used, to selectively bypass immutability checks, on a per attribute basis.  By default,
+     * it just returns false.  If it returns true, we will allow the attribute to be set.
+     * 
+     * @param  string $attribute
+     * @return boolean
+     */
+    protected function bypassImmutabilityCheck( $attribute )
+    {
+        return false;
+    }
+}
